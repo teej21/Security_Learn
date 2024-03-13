d06aa7d5ff2d0ec892419d7f05daef701bd50eb0
Teej <vinhnek117@gmail.com>
2024-03-13 15:08:25 +0700

    Initial commit

---
+++ src/main/java/org/example/security_learn/SecurityLearnApplication.java
@@ -0,0 +1,13 @@
+package org.example.security_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityLearnApplication.class, args);
+    }
+
+}
